be40b6bda369507f8bfd100cabe4a4223b29d428
thendralprasath <thendralprasath@gmail.com>
2021-08-10 14:28:53 +0530

    updatedredbus

--- src/Red.java
+++ src/Red.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Red {
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("changes...............");
+		System.out.println("changes...............");
+		System.out.println("changes...............");
 	       System.setProperty("webdriver.chrome.driver","C:\\Users\\ARI\\Desktop\\chromee\\chromedriver.exe");
 	       WebDriver driver=new ChromeDriver();
 	       driver.get("https://www.redbus.in/" );
@@ -21,7 +24,9 @@ public class Red {
 	      robo.click();
 	      WebElement otp= driver.findElement(By.xpath("(//span[@class='f-w-b'])[3]"));
 	      otp.click();
-	
+	      System.out.println("changes...............");
+			System.out.println("changes...............");
+			System.out.println("changes...............")
 	}
 	
 }
